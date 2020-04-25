0c90124489f8d08176314baa4a5b02e8af14c278
name <canderson109@mail.greenriver.edu>
2020-04-25 01:30:50 -0400

    added php blocks

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+/*
+ * Name:  Conner Anderson
+ * Date:  4/24/2020
+ * URL:   https://canderson.greenriverdev.com/328/pp2/index.php
+ * Title: Pair Program 2
+ */
+
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>PP2</title>
+</head>
+<body>
+
+<?php
+echo "<h1>PHP Array Practice</h1>"
+
+?>
+
+</body>
+</html>
